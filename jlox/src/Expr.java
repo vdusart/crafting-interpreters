5f6b2fcba9c3c51d5a0c4d9e6cd17244127a50e5
Victor DUSART <dusartvictor@gmail.com>
2024-01-06 16:17:40 +0100

    improve generator to implement Visitor Design Pattern

--- jlox/src/Expr.java
+++ jlox/src/Expr.java
@@ -1,6 +1,15 @@
 import java.utils.List;
 
 abstract class Expr {
+	interface Visitor<R> {
+		R visitBinaryExpr(Binary expr);
+		R visitGroupingExpr(Grouping expr);
+		R visitLiteralExpr(Literal expr);
+		R visitUnaryExpr(Unary expr);
+	}
+
+	abstract <R> R accept(Visitor<R> visitor);
+
 	static class Binary extends Expr {
 		final Expr left;
 		final Token operator;
@@ -11,6 +20,11 @@ abstract class Expr {
 			this.operator = operator;
 			this.right = right;
 		}
+
+		@Override
+		<R> R accept(Visitor<R> visitor) {
+			return visitor.visitBinaryExpr(this);
+		}
 	}
 	static class Grouping extends Expr {
 		final Expr expression;
@@ -18,12 +32,22 @@ abstract class Expr {
 		Grouping(Expr expression) {
 			this.expression = expression;
 		}
+
+		@Override
+		<R> R accept(Visitor<R> visitor) {
+			return visitor.visitGroupingExpr(this);
+		}
 	}
 	static class Literal extends Expr {
 		final Object value;
 
 		Literal(Object value) {
 			this.value = value;
+		}
+
+		@Override
+		<R> R accept(Visitor<R> visitor) {
+			return visitor.visitLiteralExpr(this);
 		}
 	}
 	static class Unary extends Expr {
@@ -33,6 +57,11 @@ abstract class Expr {
 		Unary(Token operator, Expr right) {
 			this.operator = operator;
 			this.right = right;
+		}
+
+		@Override
+		<R> R accept(Visitor<R> visitor) {
+			return visitor.visitUnaryExpr(this);
 		}
 	}
 }
--- tool/generate_ast.py
+++ tool/generate_ast.py
@@ -24,21 +24,41 @@ def defineType(writer: Writer, className: str, fields: str):
     writer.write(f"{className}({fields}) {{", nbIndentations=2)
 
     # Store parameters in fields.
-    
     for field in fieldsList:
         fieldName = field.split(' ')[1]
         writer.write(f"this.{fieldName} = {fieldName};", nbIndentations=3)
     writer.write("}", nbIndentations=2)
+
+    # Visitor pattern.
+    writer.write('')
+    writer.write('@Override', nbIndentations=2)
+    writer.write("<R> R accept(Visitor<R> visitor) {", nbIndentations=2)
+    writer.write(f"return visitor.visit{className}{BASE_NAME}(this);", nbIndentations=3)
+    writer.write("}", nbIndentations=2)
+
     writer.write("}", nbIndentations=1)
+
+def defineVistor(writer: Writer, types: list[str]):
+    writer.write("interface Visitor<R> {", nbIndentations=1)
+
+    for type in types:
+        typeName = type.split(':')[0].strip()
+        writer.write(f"R visit{typeName}{BASE_NAME}({typeName} {BASE_NAME.lower()});", nbIndentations=2)
+    writer.write("}\n", nbIndentations=1)
 
 def defineAST(types: list[str]):
     path = f"{OUTPUT_DIR}/{BASE_NAME}.java"
     f = open(path, 'w')
     writer = Writer(f)
 
-    writer.write('import java.utils.List;')
-    writer.write('')
+    writer.write('import java.utils.List;\n')
     writer.write(f"abstract class {BASE_NAME} {{")
+
+    defineVistor(writer, types)
+
+    # The base accept() method.
+    writer.write("abstract <R> R accept(Visitor<R> visitor);", nbIndentations=1)
+    writer.write('')
 
     # The AST classes.
     for type in types:
